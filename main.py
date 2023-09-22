87b5e36019aff340e46b98f5362c0e9418eb1371
genemate <110731423+genemate@users.noreply.github.com>
2023-09-22 13:43:18 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,8 @@
+
+year = input("What year you entered WIUT?")
+ent_year = 2023
+
+if int(year) == ent_year:
+    print("we entered in the same year!")
+else:
+    print("You enterred Wiut", ent_year-int(year), "years ago")
